9c671b8168872017c3fbf0f29265614eab574532
prabhupadapanda12-blip <prabhupadapanda74@gmail.com>
2026-06-09 23:24:11 +0530

    LearningPython

---
+++ Division.py
@@ -0,0 +1,8 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+if b != 0:
+    result = a / b
+    print("Division =", result)
+else:
+    print("Division by zero is not allowed")
